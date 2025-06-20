393e0ef5f15ca968160a259a6ed026a3394dbb61
yurasulima <yurasulima124@gmail.com>
2025-06-21 01:12:18 +0300

    fix locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,14 @@
 import { createI18n } from 'vue-i18n'
 import en from '@/locales/en.json'
-import ua from '@/locales/ua.json'
+import uk from '@/locales/uk.json'
 import ru from '@/locales/ru.json'
-const savedLang = localStorage.getItem('lang') || 'ua'
+const savedLang = localStorage.getItem('lang') || 'en'
 const i18n = createI18n({
     locale: savedLang,
     fallbackLocale: 'en',
     messages: {
         en,
-        ua,
+        uk,
         ru,
     }
 })
